5969fd8ae85ae93a511557f36a05f190c6bfccf2
Shane99dt <dushen1999thilaksha@gmail.com>
2022-07-07 15:21:43 +0200

    add task  and delete task

--- script.js
+++ script.js
@@ -8,32 +8,6 @@ const form = document.getElementById("newTaskForm")
 const input = document.getElementById("newTaskInput")
 const tasksList = document.getElementById("tasks")
 
-// const createTask = () => {
-//   tasks.innerHTML += `
-//     <div id="tasks" class="taskDiv">
-
-//       <i id="validate" class='fa fa-circle' style='color:#b5b5b5'></i>
-//       <div class="taskContent">
-//           sosmdfjh
-//       </div>
-//       <div class="iconsEnd">
-//         <i id="priority" class='fa fa-star' style='color:#d35a78'></i>
-//         <i id="sendToDoing" class='fa fa-arrow-down' style='color:#740707'></i>
-//         <i id="deleteClass" class='fa-solid fa-trash-can'></i>
-//       </div>
-
-//     </div>
-//     `
-// }
-
-// document.querySelector('#addTask').addEventListener('click', () => {
-//   createTask()
-// });
-
-// const deleteTaskFunction = () => {
-
-// }
-
 // prevents refreshing the page while adding a task
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -45,32 +19,36 @@ form.addEventListener("submit", (e) => {
     return
   }
 
-  // tasks.innerHTML += `
-  // <div class="taskDiv" id="taskEl">
-  //   <i id="validate" class='fa fa-circle' style='color:#b5b5b5'></i>
-  //   <div class="taskContent">
-  //     <input type = "text" value = "${task}" readonly>
-  //   </div>
-  //   <div class="iconsEnd">
-  //     <i id="priority" class='fa fa-star' style='color:#d35a78'></i>
-  //     <i id="sendToDoing" class='fa fa-arrow-down' style='color:#740707'></i>
-  //     <i id="deleteClass" class='fa-solid fa-trash-can'></i>
-  //     <button id="editBtn">edittt</button>
-  //   </div>
-  // </div>
-  // `
+//  // tasks.innerHTML += `
+//  // <div class="taskDiv" id="taskEl">
+//  //   <i id="validate" class='fa fa-circle' style='color:#b5b5b5'></i>
+//  //   <div class="taskContent">
+//  //     <input type = "text" value = "${task}" readonly>
+//  //   </div>
+//  //   <div class="iconsEnd">
+//  //     <i id="priority" class='fa fa-star' style='color:#d35a78'></i>
+//  //     <i id="sendToDoing" class='fa fa-arrow-down' style='color:#740707'></i>
+//  //     <i id="deleteTask" class='fa-solid fa-trash-can'></i>
+//  //     <button id="editBtn">edittt</button>
+//  //   </div>
+//  // </div>
+//  // `
   const taskElement = document.createElement('div')
   taskElement.classList.add('taskDiv')
 
   const buttonValidate = document.createElement('button')
   const validateImg = document.createElement('i')
 
+  taskElement.appendChild(buttonValidate)
   buttonValidate.appendChild(validateImg)
+
   validateImg.classList.add('fa', 'fa-circle')
   validateImg.setAttribute('id', 'validate')
 
   const taskContentElement = document.createElement('div')
   taskContentElement.classList.add('taskContent')
+
+  taskElement.appendChild(taskContentElement)
 
   const contentPara = document.createElement('p')
   contentPara.classList.add('content')
@@ -96,28 +74,26 @@ form.addEventListener("submit", (e) => {
   doingTaskImg.classList.add('fa', 'fa-arrow-down')
   doingTaskImg.setAttribute('id', 'priority')
 
+  const buttonDelete = document.createElement('button')
+  const deleteTaskImg = document.createElement('i')
 
+  buttonDelete.appendChild(deleteTaskImg)
+  deleteTaskImg.classList.add('fa-solid', 'fa-trash-can')
+  deleteTaskImg.setAttribute('id', 'deleteTaskImg')
+
+
+  taskElement.appendChild(buttonPriority)
+  taskElement.appendChild(buttonDoingTask)
+  taskElement.appendChild(buttonDelete)
+  tasksList.appendChild(taskElement)
 
   // rest the value of the task
   input.value =''
 
-  // const taskEdit = document.getElementById('editBtn')
-
-  // let id = this.getAttribute('id')
-  // console.log(id)
-
-  // taskEdit.addEventListener('click', () => {
-  //   if( id == ("editBtn")){
-  //     console.log("ok")
-  //   }
-  // })
-  const deleteTask = document.getElementById("deleteClass")
-
-
-  // delete task
-  // deleteTask.addEventListener('click', () => {
-  //   tasks.removeChild(document.getElementById('taskEl'))
-  // })
+  // delete a task
+  buttonDelete.addEventListener('click', () => {
+    tasksList.removeChild(taskElement)
+  })
 
 })
 
