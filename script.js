98cb52383f69502fabcac7f2b588f589a70e7c1f
Shane99dt <dushen1999thilaksha@gmail.com>
2022-07-07 12:14:21 +0200

    add task button

--- script.js
+++ script.js
@@ -9,27 +9,27 @@ const form = document.getElementById("newTaskForm")
 const input = document.getElementById("newTaskInput")
 const tasks = document.getElementById("tasks")
 
-const createTask = () => {
-  tasks.innerHTML += `
-    <div id="tasks" class="taskDiv">
+// const createTask = () => {
+//   tasks.innerHTML += `
+//     <div id="tasks" class="taskDiv">
 
-      <i id="validate" class='fa fa-circle' style='color:#b5b5b5'></i>
-      <div class="taskContent">
-          sosmdfjh
-      </div>
-      <div class="iconsEnd">
-        <i id="priority" class='fa fa-star' style='color:#d35a78'></i>
-        <i id="sendToDoing" class='fa fa-arrow-down' style='color:#740707'></i>
-        <i id="deleteClass" class='fa-solid fa-trash-can'></i>
-      </div>
+//       <i id="validate" class='fa fa-circle' style='color:#b5b5b5'></i>
+//       <div class="taskContent">
+//           sosmdfjh
+//       </div>
+//       <div class="iconsEnd">
+//         <i id="priority" class='fa fa-star' style='color:#d35a78'></i>
+//         <i id="sendToDoing" class='fa fa-arrow-down' style='color:#740707'></i>
+//         <i id="deleteClass" class='fa-solid fa-trash-can'></i>
+//       </div>
 
-    </div>
-    `
-}
+//     </div>
+//     `
+// }
 
-document.querySelector('#addTask').addEventListener('click', () => {
-  createTask()
-});
+// document.querySelector('#addTask').addEventListener('click', () => {
+//   createTask()
+// });
 
 // const deleteTaskFunction = () => {
 
@@ -45,6 +45,34 @@ form.addEventListener("submit", (e) => {
     alert("Please add a task")
     return
   }
+
+  tasks.innerHTML += `
+  <div class="taskDiv">
+    <i id="validate" class='fa fa-circle' style='color:#b5b5b5'></i>
+    <div class="taskContent">
+      <input type = "text" value = "${task}" readonly>
+    </div>
+    <div class="iconsEnd">
+      <i id="priority" class='fa fa-star' style='color:#d35a78'></i>
+      <i id="sendToDoing" class='fa fa-arrow-down' style='color:#740707'></i>
+      <i id="deleteClass" class='fa-solid fa-trash-can'></i>
+      <button id="editBtn">edittt</button>
+    </div>
+  </div>
+  `
+  // rest the value of the task
+  input.value =''
+
+  const taskEdit = document.getElementById('editBtn')
+
+  let id = this.getAttribute('id')
+  console.log(id)
+
+  taskEdit.addEventListener('click', () => {
+    if( id == ("editBtn")){
+      console.log("ok")
+    }
+  })
 
 
 })
